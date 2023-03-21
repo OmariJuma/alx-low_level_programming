f5fdaa0ac5f6f6110bbf17cbd994548ac191e66c
Omar <juma02669@gmail.com>
2023-03-21 16:04:23 -0700

    Putchar to the console

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "_putchar" followed by a new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
