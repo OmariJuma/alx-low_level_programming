c108a4a328356c45485e2f3e1355c87d3315f067
Omar <juma02669@gmail.com>
2023-04-10 12:22:22 -0700

    To infinity and beyond Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,13 @@
  * @argc: number of commandline arguments
  * @argv: list of commandline arguments
  *
- * Return: 0 Program is Always success
+ * Return: 0 Always(success)
  */
 int main(int argc, char **argv)
 {
 	printf("%s\n", argv[0]);
+
 	(void)argc;
+
 	return (0);
 }
