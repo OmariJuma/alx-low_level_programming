144086c7c134adce9f0f6bb54c5bf818bf59b417
Omar <juma02669@gmail.com>
2023-04-12 12:29:17 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,27 +2,49 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - nested loop to make grid
- * @width: width input
- * @height: height input
- * Return: pointer to 2 dim
+ * alloc_grid - creates 2d width * height array of integers
+ * @width: width of array
+ * @height: height of array
+ * Each element of the grid should be initialized to 0
+ *
+ * Return: pointer to the created array
+ * If width or height is 0 or negative, return NULL
+ * The function should return NULL on failure
  */
 int **alloc_grid(int width, int height)
 {
+	int **grid;
+	int i, j;
 
-int **a;
-int x, y;
+	/* if width or height is 0 or negative, return NULL */
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-if (width <= 0 || height <= 0)
-{
-return (NULL);
+	/* create an array of pointers */
+	grid = (int **)malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+
+			free(grid);
+
+			return (NULL);
+		}
+	}
+
+	/* initialize all values in the array to 0 */
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+
+	return (grid);
 }
-a = malloc(sizeof(int *) * height);
-
-if (a == NULL)
-return (NULL);
-
-for (x = 0; x < height; x++)
-{
-a[x] = malloc(sizeof(int) * width);
-
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,21 +1,18 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * free_grid - frees 2d array
- * @grid: 2d grid
- * @height: height of grid
- * Return: nothing
+ * free_grid - frees a 2 dimensional grid
+ * previously created by the alloc_grid function
+ * @grid: grid to be freed
+ * @height: height of the grid
  */
 void free_grid(int **grid, int height)
 {
+	int i;
 
-int c;
+	for (i = 0; i < height; i++)
+		free(grid[i]);
 
-for (c = 0; c < height; c++)
-{
-free(grid[c]);
-}
-free(grid);
+	free(grid);
 }
